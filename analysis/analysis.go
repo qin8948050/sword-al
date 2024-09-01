60c1542e393f8412950bfb4b90b27d2502e00ad2
qin8948050 <275097304@qq.com>
2024-09-01 15:38:36 +0800

    ✨ feat(analysis): 合并区间

--- analysis/analysis.go
+++ analysis/analysis.go
@@ -1,6 +1,9 @@
 package analysis
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func PrintMatrixInCircle(matrix [][]int, rows, columns int) {
 	if rows == 0 && columns == 0 {
@@ -66,6 +69,91 @@ func MaxSubArray(numbers []int) []int {
 	for i := maxCount - 1; i >= 0; i-- {
 		result[i] = maxKey
 		maxKey--
+	}
+	return result
+}
+
+// 最长子序列,忽略元素出现的顺序
+// a:=[]int{7,1,4,3,5,9,10,25,11,12,33,2,13,6}
+func MaxSubArray1(numbers []int) []int {
+	if len(numbers)<=0{
+		return numbers
+	}
+	m:=make(map[int]int,0)
+	for i:=0;i<len(numbers);i++ {
+		currentNumber:=numbers[i]
+		if _,ok:=m[currentNumber];ok{
+			continue
+		} else if previousValue,ok:=m[currentNumber-1];ok{
+			m[currentNumber]=previousValue+1
+			delete (m,currentNumber-1)
+		} else {
+			m[currentNumber]=1
+		}
+	}
+
+	
+	replay:
+    for k,v:=range m {
+		if count,ok:=m[k-v];ok{
+				m[k]=v+count
+				delete(m,k-1)
+		}
+		if count,ok:=m[k-v];ok{
+			m[k]=v+count
+			delete(m,k-1)
+			goto replay
+		}
+	}
+
+	maxCount:=0
+	var maxKey int
+	for k,v:=range m {
+		if v>maxCount{
+			maxCount=v
+			maxKey=k
+		}
+	}
+	result:=make([]int,maxCount)
+	for i:=maxCount-1;i>=0;i-- {
+		result[i]=maxKey
+		maxKey--
+	}
+	return result
+}
+
+// 合并区间
+func BubbleSort(data [][]int) {
+	for i:=0;i<len(data)-1;i++ {
+		for j:=0;j<len(data)-1-i;j++ {
+			if  data[j][0]>data[j+1][0] || (data[j][0]==data[j][0] && data[j][1]>data[j+1][1]){
+				data[j],data[j+1]=data[j+1],data[j]
+			}
+		}
+	}
+}
+
+func Merge(ranges [][]int) [][]int{
+	if len(ranges)<=1 {
+		return ranges
+	}
+	BubbleSort(ranges)
+	result:=[][]int{{ranges[0][0],ranges[0][1]}}
+	for i:=1;i<len(ranges);{
+		l1:=result[len(result)-1][0]
+		u1:=result[len(result)-1][1]
+		l2:=ranges[i][0]
+		u2:=ranges[i][1]
+		if u1>=l2{
+			sub:=[]int{l1,int(math.Max(float64(u1),float64(u2)))}
+			if len(result)>0 {
+				result=result[:len(result)-1]
+			}
+			result=append(result, sub)
+		}else {
+			result=append(result, []int{l1,u1},[]int{l2,u2})
+		}
+		i++
 	}
 	return result
 }
