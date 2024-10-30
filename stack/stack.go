783cc4d48cc1fe35679092280b08859aba09ac93
xwqin <xwqin@gaojihealth.com>
2024-10-30 10:34:50 +0800

    🐞 fix(stack): 两个栈实现队列

--- stack/stack.go
+++ stack/stack.go
@@ -1,30 +1,47 @@
 package stack
 
 type Queue struct {
-	Stack1 []int
-	Stack2 []int
+   StackIn []int
+   StackOut []int
 }
 
-func(q *Queue)Insert(d int)  {
-	q.Stack1 = append(q.Stack1,d)
+func(queue *Queue)Enqueue(number int) {
+   queue.StackIn=append(queue.StackIn, number)
 }
 
-func(q *Queue)Delete() int{
-	var result int
-	if len(q.Stack2)<=0{
-		for len(q.Stack1)>0 {
-		q.Stack2 = append(q.Stack2, q.Stack1[len(q.Stack1)-1])
-		q.Stack1=q.Stack1[:len(q.Stack1)-1]
-		}
-	}
-	if len(q.Stack2)>0{
-		result=q.Stack2[len(q.Stack2)-1]
-		q.Stack2=q.Stack2[:len(q.Stack2)-1]
-	} else {
-		panic("empty")
-	}
-	return result
+func(queue *Queue)DeQueue() int {
+   if len(queue.StackIn)>0{
+      for len(queue.StackIn)>0 {
+         top:=queue.StackIn[len(queue.StackIn)-1]
+         queue.StackOut=append(queue.StackOut, top)
+         queue.StackIn=queue.StackIn[:len(queue.StackIn)-1]
+      }
+   }
+   
+   if len(queue.StackOut)==0 {
+      panic("queue empty")
+   } 
+   result:=queue.StackOut[len(queue.StackOut)-1]
+   queue.StackOut=queue.StackOut[:len(queue.StackOut)-1]
+   return result
 }
+
+func(queue *Queue)Front() int {
+   if len(queue.StackIn)>0{
+      for len(queue.StackIn)>0 {
+         top:=queue.StackIn[len(queue.StackIn)-1]
+         queue.StackOut=append(queue.StackOut, top)
+         queue.StackIn=queue.StackIn[:len(queue.StackIn)-1]
+      }
+   }
+   
+   if len(queue.StackOut)==0 {
+      panic("queue empty")
+   } 
+   result:=queue.StackOut[len(queue.StackOut)-1]
+   return result
+}
+
 
 type Stack struct {
 	Queue1 []int
@@ -45,6 +62,20 @@ func(s *Stack)Pop() int{
 	}
 	result:=s.Queue1[0]
 	s.Queue1, s.Queue2 = s.Queue2, s.Queue1[:0]
+	return result
+}
+
+func(s *Stack)Top() int {
+	if len(s.Queue1)==0  {
+		panic("stack empty")
+	}
+	var result int
+	for len(s.Queue1)>0 {
+		result=s.Queue1[0]
+		s.Queue2=append(s.Queue2, s.Queue1[0])
+		s.Queue1=s.Queue1[1:]
+	}
+	s.Queue1, s.Queue2 = s.Queue2, s.Queue1
 	return result
 }
 
