e0c9d04118b002ba2432fa4213e4027c4aeedfd1
xwqin <xwqin@gaojihealth.com>
2024-11-01 15:55:44 +0800

    🐞 fix(bit): 数组中只出现一次的两个数，其他都出现两次

--- bit/bit.go
+++ bit/bit.go
@@ -27,25 +27,26 @@ func CountOnesInBinaryCount(n int) int {
 	return count
 }
 
+// 用来检查一个整数 n 是否是二的幂
+// 2的幂，二进制表示只有1个1
+// n-1: n的唯一的1位变为 0，其余的0位变为1
+// n&(n-1) 结果为0
 func IsExponseOfTwo(n int) bool {
-	count := 0
-	for n > 0 {
-		count++
-		n = n & (n - 1)
-	}
-	return count == 1
+	return n>0&& (n&(n-1))==0
 }
 
 // 数组中只出现一次的两个数，其他都出现两次
 func FindTwoSingleNumbers(numbers []int) (int,int) {
    xor:=0
+   //1.计算整体异或结果，重复的数字会相互抵消，最后只剩出现一次数的异或结果
    for _,n:=range numbers {
       xor^=n
    }
-   //将数分为两组
+   //2.找差异位，不同组的数分为出现在1和0的不同位置
    diff:=xor&(-xor)
    num1:=0
    num2:=0
+   //3.分组
    for _,n:=range numbers {
       //1组
       if n & diff==1 {
